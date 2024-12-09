bb43f56f8bb0aee50d47b84f79b9a15bd53856be
张粤圳 <zhangyuezhen@yhmsi.com>
2024-12-09 15:03:31 +0800

    feat: lesson1<Initial express>

---
+++ server/src/app.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json()); // 解析 JSON 请求体
+app.use(express.urlencoded({ extended: true })); // 解析 URL 编码的请求体
+
+// 基本路由
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Express with TypeScript!");
+});
+
+// 处理 GET 请求
+app.get("/get-data", (req: Request, res: Response) => {
+  res.send("GET request received");
+});
+
+// 处理 POST 请求
+app.post("/post-data", (req: Request, res: Response) => {
+  res.send("POST request received");
+});
+
+app.get("/user/:id", (req: Request, res: Response) => {
+  const userId = req.params.id;
+  res.send(`User ID: ${userId}`);
+});
+
+app.get("/search", (req: Request, res: Response) => {
+  const { name, age } = req.query;
+  res.send(`Search results for ${name}, age: ${age}`);
+});
+
+// 启动服务器，监听 3000 端口
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
