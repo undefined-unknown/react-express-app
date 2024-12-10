b57efcc10360e51d78d6d3b6b00a845740587501
张粤圳 <zhangyuezhen@yhmsi.com>
2024-12-10 14:03:54 +0800

    feat: lesson3<mongoose operation>

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -2,11 +2,17 @@ import { Router, Request, Response } from "express";
 import mongoose from "mongoose";
 
 // 定义 User Schema
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true },
+    delete_flag: { type: String, default: 0 },
+  },
+  {
+    timestamps: true, // 自动创建 createdAt 和 updatedAt 字段
+  }
+);
 
 // 创建 User 模型
 const User = mongoose.model("User", userSchema);
@@ -42,7 +48,7 @@ router.post("/users", async (req: Request, res: Response) => {
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({ message: "Failed to create user" });
+    res.status(500).json({ message: "Failed to create user", error });
   }
 });
 
@@ -58,19 +64,24 @@ router.put("/users/:id", async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Failed to update user" });
+    res.status(500).json({ message: "Failed to update user", error });
   }
 });
 
 router.delete("/users/:id", async (req: Request, res: Response) => {
   try {
-    const updatedUser = await User.findByIdAndDelete(req.params.id);
+    // 软删除
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: { delete_flag: "1" } },
+      { new: true }
+    );
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found" });
     }
     res.json({ message: "User deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Failed to delete user" });
+    res.status(500).json({ message: "Failed to delete user", error });
   }
 });
 
